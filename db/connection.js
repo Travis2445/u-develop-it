08a34146e6f885b70de344def68ab2bcec8f026d
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2022-01-08 00:55:12 -0600

    redo of repo initial commit

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+// Connect to databasse
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySql username
+        user: 'travis',
+        // your MySql password
+        password: 'pew',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
